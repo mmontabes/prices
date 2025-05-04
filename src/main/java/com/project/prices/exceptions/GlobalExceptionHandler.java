a31af54d8d0b43af8857131d8eb2f7046c9a659e
Miguel <miguel.montabes@gmail.com>
2025-05-05 01:12:41 +0200

    exception added

--- src/main/java/com/project/prices/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/project/prices/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.project.prices.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +16,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Malformed input: " + ex.getMessage());
+    }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
         if(ex instanceof MissingServletRequestParameterException){
